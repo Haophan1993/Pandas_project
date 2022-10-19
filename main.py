29da1abdbd711e4d76efce4f2da78440d44be48b
Hao Phan <haophan120193@gmail.com>
2022-10-19 15:00:39 -0400

    10/19/2022

--- main.py
+++ main.py
@@ -4,9 +4,19 @@ import numpy as ny
 import requests
 
 API_KEY='AIzaSyBQvFKIQCnFRw7YN7U2b6c5iTzhKzrpHFs'
-url='https://www.googleapis.com/youtube/v3/search?part=snippet,id&q=Toyota&maxResults=20&order=date&pageToken=''&key='+API_KEY
+url='https://www.googleapis.com/youtube/v3/search?part=snippet,id&q=Toyota&maxResults=5&order=date&pageToken=''&key='+API_KEY
 response=requests.get(url).json()
+print('Working on dev branch')
 
-print(response['items'])
+
+for video in response['items']:
+    
+    video_id=video['id']['videoId']
+    video_title = video['snippet']['title']
+    upload_date= str(video['snippet']['publishedAt']).split('T')[0]
+    print(video_id)
+    print(video_title)
+    print(upload_date)
+    print()
 
 
