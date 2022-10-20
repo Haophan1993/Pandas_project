659122a7aed62cc39f7b908cef833e04d805ef26
Hao Phan <haophan120193@gmail.com>
2022-10-20 13:59:09 -0400

    Last Update

--- main.py
+++ main.py
@@ -3,51 +3,6 @@ import pandas as pd
 import requests
 import psycopg2 as ps
 
-'''
-API_KEY='AIzaSyBQvFKIQCnFRw7YN7U2b6c5iTzhKzrpHFs'
-url='https://www.googleapis.com/youtube/v3/search?part=snippet,id&q=Toyota&maxResults=50&order=date&pageToken=''&key='+API_KEY
-response=requests.get(url).json()
-print('Working on dev branch')
-
-df=pd.DataFrame(columns=['VideoId','VideoTitle', 'UploadDate','ViewCount', 'LikeCount','DislikeCount', 'FavoriteCount','CommentCount'])
-
-for video in response['items']:
-    
-    video_id=video['id']['videoId']
-    video_title = video['snippet']['title']
-    upload_date= str(video['snippet']['publishedAt']).split('T')[0]
-    details_url ='https://www.googleapis.com/youtube/v3/videos?part=statistics&id='+video_id + '&key='+API_KEY
-    details_response = requests.get(details_url).json()
-    view_count=0
-    like_count=0
-    dislike_count=0
-    favorite_count=0
-    comment_count=0
-    #print(details_response['items'][0]['statistics'])
-
-    for statis in details_response['items'][0]['statistics']:
-        
-        if statis=='viewCount':
-            view_count=details_response['items'][0]['statistics']['viewCount']
-        elif statis=='likeCount':
-            like_count=details_response['items'][0]['statistics']['likeCount']
-        elif statis=='dislikeCount':
-            dislike_count=details_response['items'][0]['statistics']['dislikeCount']
-        elif statis=='favoriteCount':
-            favorite_count=details_response['items'][0]['statistics']['favoriteCount']
-        elif statis=='commentCount':
-            comment_count=details_response['items'][0]['statistics']['commentCount']
-        
-    dt=[video_id, video_title, upload_date, view_count, like_count, dislike_count, favorite_count, comment_count]    
-    df=pd.concat([df,pd.DataFrame([dt], columns=['VideoId','VideoTitle', 'UploadDate','ViewCount', 'LikeCount','DislikeCount', 'FavoriteCount','CommentCount'])], ignore_index=True)
-    
-df.to_csv('out.csv')
-'''
-
-
-#df1=pd.read_csv('out.csv', index_col=0)
-#for i, row in df1.iterrows():
-#    print(row['VideoTitle'])
 
 def connect_to_db(hostName, dbName, Port, UserName, Password):
     try:
@@ -102,14 +57,51 @@ def update_data_to_table(curr, video_title, upload_date, view_count, like_count,
                         comment_count=%s
                     WHERE video_id=%s;""")
     curr.execute(update_video,(video_title, upload_date, view_count, like_count, dislike_count, favorite_count, comment_count, video_id,) )
-                            
 
 
 
+API_KEY='AIzaSyBQvFKIQCnFRw7YN7U2b6c5iTzhKzrpHFs'
+url='https://www.googleapis.com/youtube/v3/search?part=snippet,id&q=Honda&maxResults=50&order=date&pageToken=''&key='+API_KEY
+response=requests.get(url).json()
 
 
+df=pd.DataFrame(columns=['VideoId','VideoTitle', 'UploadDate','ViewCount', 'LikeCount','DislikeCount', 'FavoriteCount','CommentCount'])
 
-df1=pd.read_csv('out.csv', index_col=0)
+for video in response['items']:
+    
+    video_id=video['id']['videoId']
+    video_title = video['snippet']['title']
+    upload_date= str(video['snippet']['publishedAt']).split('T')[0]
+    details_url ='https://www.googleapis.com/youtube/v3/videos?part=statistics&id='+video_id + '&key='+API_KEY
+    details_response = requests.get(details_url).json()
+    view_count=0
+    like_count=0
+    dislike_count=0
+    favorite_count=0
+    comment_count=0
+    
+
+    for statis in details_response['items'][0]['statistics']:
+        
+        if statis=='viewCount':
+            view_count=details_response['items'][0]['statistics']['viewCount']
+        elif statis=='likeCount':
+            like_count=details_response['items'][0]['statistics']['likeCount']
+        elif statis=='dislikeCount':
+            dislike_count=details_response['items'][0]['statistics']['dislikeCount']
+        elif statis=='favoriteCount':
+            favorite_count=details_response['items'][0]['statistics']['favoriteCount']
+        elif statis=='commentCount':
+            comment_count=details_response['items'][0]['statistics']['commentCount']
+        
+    dt=[video_id, video_title, upload_date, view_count, like_count, dislike_count, favorite_count, comment_count]    
+    df=pd.concat([df,pd.DataFrame([dt], columns=['VideoId','VideoTitle', 'UploadDate','ViewCount', 'LikeCount','DislikeCount', 'FavoriteCount','CommentCount'])], ignore_index=True)
+
+
+#df.to_csv('out.csv')
+
+
+#df=pd.read_csv('out.csv', index_col=0)
 
 host_name='mydatabase-instance.cmbb90ngezbh.us-east-1.rds.amazonaws.com'
 dbname='first_test_db'
@@ -122,9 +114,10 @@ conn = connect_to_db(host_name,dbname, port, username, password)
 curr=conn.cursor()
 create_table(curr)
 
-for i, row in df1.iterrows():
+for i, row in df.iterrows():
+    
+    if check_exist_video(curr, row['VideoId']) ==None:
 
-    if check_exist_video(curr, row['VideoId']) =='':
         insert_data_to_table(curr, row['VideoId'], row['VideoTitle'], row['UploadDate'], row['ViewCount'], row['LikeCount'], 
                            row['DislikeCount'], row['FavoriteCount'], row['CommentCount'])
     
